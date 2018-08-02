b26fbafaaf7172712b3faf310c54cd2870d41ba4
Yuki Yoshimura <cy15161@shibaura-it.ac.jp>
2018-08-02 18:06:14 +0000

    practice of makeing gem file

---
+++ lib/my_rubygem.rb
@@ -0,0 +1,7 @@
+require "my_rubygem/version"
+
+module MyRubygem
+    def self.greet
+        'Hello World!'
+    end
+end
---
+++ spec/my_rubygem_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe MyRubygem do
+  it "has a version number" do
+    expect(MyRubygem::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
